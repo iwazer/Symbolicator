dfe65eeb0a83ad51646ca8315be4bb3901a0b285
iwazer <iwazawa@gmail.com>
2015-09-16 18:44:24 +0900

    Initial Commit

---
+++ Symbolicator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Symbolicator
+//
+//  Created by 岩澤 英治 on 2015/09/16.
+//  Copyright (c) 2015年 iwazer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
